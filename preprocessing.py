f6c020d11efdc299cf9e9625f45403176dd4fbc6
Amosse Edouard <eamosse@gmail.com>
2017-01-24 15:16:04 +0100

    First commit

---
+++ preprocessing.py
@@ -0,0 +1,9 @@
+from helper import Word2VecHelper, FileHelper
+classes = []
+
+
+
+if __name__ == '__main__':
+    files = ["./train/dbpedia/generic/positive.txt", "./train/dbpedia/generic/negative.txt"]
+    Word2VecHelper.createModel(files, "dbpedia_generic")
+    #FileHelper.createTrainFile(FileHelper.categories, directory="train/dbpedia/generic", name="dbpedia_generic_train")
