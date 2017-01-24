c92a607e12a3538555ff94f0db34719a7b1f0d09
Amosse Edouard <eamosse@gmail.com>
2017-01-24 18:37:47 +0100

    Implement setting 2 (i.e train and test on different dataset)

--- preprocessing.py
+++ preprocessing.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
     files = ["./train/dbpedia/generic/positive.txt", "./train/dbpedia/generic/negative.txt"]
     #Word2VecHelper.createModel(files, "dbpedia_generic")
     FileHelper.createTrainFile(FileHelper.categories, directory="train/dbpedia/generic", name="dbpedia_generic_train")
+    FileHelper.createTrainFile(FileHelper.categories, directory="test/dbpedia/generic", name="dbpedia_generic_test")
